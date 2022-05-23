6cc75b312ddde9fa4b1f20ee3deadf1bd7f8ddb7
KaiWhatmore <kai.whatmore@medirecords.com>
2022-05-23 11:09:24 +1000

    main: add Valid-Anagrams

---
+++ 242-Valid-Anagrams.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        checkAnagramS = {}
+        checkAnagramT = {}
+
+        if len(s) != len(t):
+            return False
+
+        for letterS in s:
+            if letterS not in checkAnagramS:
+                checkAnagramS[letterS] = 1
+            else:
+                checkAnagramS[letterS] += 1
+
+        for letterT in t:
+            if letterT not in checkAnagramT:
+                checkAnagramT[letterT] = 1
+            else:
+                checkAnagramT[letterT] += 1
+
+        for valueS in checkAnagramS.keys():
+            if valueS not in checkAnagramT.keys():
+                return False
+
+        for valueS in checkAnagramS:
+            if checkAnagramS[valueS] != checkAnagramT[valueS]:
+                return False
+        return True
+          
